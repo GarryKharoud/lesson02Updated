65c897344a07f1f47c3290fcbd2dd2c2e8bcd9f4
KharoudGarry <200522622@student.georgianc.on.ca>
2023-01-19 19:55:51 -0500

    adding shuffle final commit

--- src/main/java/com/example/lesson02/DeckOfCards.java
+++ src/main/java/com/example/lesson02/DeckOfCards.java
@@ -1,6 +1,8 @@
 package com.example.lesson02;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class DeckOfCards
@@ -17,6 +19,27 @@ public class DeckOfCards
         {
             for (String faceName : faceNames)
                 deck.add(new Card(faceName,suits.get(i)));
+        }
+    }
+
+    /**
+     * dealing the top card from deck
+     */
+
+    public  void shuffle()
+    {
+        Collections.shuffle(deck);
+    }
+
+    public  Card dealTopCard()
+    {
+        if(deck.size()>0)
+        {
+            return  deck.remove(0);
+        }
+        else
+        {
+            return null;
         }
     }
 }
--- src/main/java/com/example/lesson02/Main.java
+++ src/main/java/com/example/lesson02/Main.java
@@ -1,5 +1,7 @@
 package com.example.lesson02;
 
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args)
@@ -10,5 +12,18 @@ public class Main
         System.out.println(card1+" value: "+card1.getCardValue());
 
         DeckOfCards deck = new DeckOfCards();
+        deck.shuffle();
+        //for (Card)
+        ArrayList<Card> hand1 = new ArrayList<>();
+        ArrayList<Card> hand2 = new ArrayList<>();
+
+        for (int i=1;i<=5;i++)
+        {
+            hand1.add(deck.dealTopCard());
+            hand2.add(deck.dealTopCard());
+        }
+
+        System.out.println("Hand 1: "+hand1);
+        System.out.println("Hand 2: "+hand2);
     }
 }
